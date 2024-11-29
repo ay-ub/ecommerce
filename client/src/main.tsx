1698839e786cb691294f516f40e8e93ad8632bc8
Ay-ob <ayoubhadjyoucef1@gmail.com>
2024-11-29 12:58:01 +0100

    feat: add Toaster to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "./i18n";
+import { Toaster } from "sonner";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>
 );
